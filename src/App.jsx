2e0816e5f88b291c4389f136b66e8d44d115b94e
Marwan Mohamed <mac@192.168.1.90>
2026-05-09 07:33:57 -0700

    basic static done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
 import Nav from "./components/Nav";
+import { FaGithub } from "react-icons/fa";
+
 
 
 export default function App(){
   const [isOpen , setIsOpen]=useState(false)
 
   return(
-    <div className="min-h-screen max-sm space-y-6 p-6">
+    <div className="min-h-screen max-sm space-y-10 p-6">
       <Nav/>
+
       <div className="space-y-6">
       <h1 className="font-semibold text-lg tracking-widest">FOR LEARNING</h1>
       <p className="font-semibold text-2xl text-blue-900 tracking-widest leading-9 ">Front-end and full-stack coding challenges that build real skills and help get you hired</p>
       <p className="text-gray-500 tracking-wider leading-8 text-lg">Frontend Mentor provides professional design-to-code challenges that mirror real-world development work. Join <span className="text-blue-900 font-bold">1,158,938</span> developers building portfolio projects that impress employers.</p>
       <div className="flex flex-col  space-y-4">
-        <button className="text-white p-2 font-bold text-sm bg-rose-500 rounded-full w-3/4 text-center">START BUILDING FREE</button>
-        <button className="font-bold w-3/5 bg-white shadow-lg  text-sm rounded-full p-2 text-center">BROWSE CHALLENGES</button>
+        <button className="text-white  py-3 font-bold text-sm bg-rose-600 rounded-full w-3/4 text-center flex items-center justify-center gap-4">START BUILDING FREE <FaGithub /></button>
+        <button className="font-bold w-3/5 bg-white shadow-lg  text-sm rounded-full p-2 text-center border border-1 border-gray-200">BROWSE CHALLENGES</button>
+      </div>
+      <div>
+        <img src="/images/home-hero.webp" alt="desktop code editor image" />
+      </div>
+      <div className="rounded-lg p-4 space-y-6 bg-blue-900">{/*for the blue container under edtior img */}
+        <h1 className="font-bold text-white">Get AI-powered feedback on every solution submission</h1>
+        <p className="text-gray-400">Join thousands of developers getting personalized insights with every submission to level up faster. </p>
+        <button className="bg-white p-2 rounded-full text-blue-800 text-center w-full">LEARN ABOUT PRO</button>
       </div>
       </div>
+
 
 
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaGithub } from "react-icons/fa";
 
 export default function Nav(){
     const [isOpen , setIsOpen]=useState(false)
@@ -11,17 +12,17 @@ export default function Nav(){
 
   return(
     <div className="flex justify-between ">
-      <div className="flex gap-4  items-center relative w-full">
+      <div className="flex gap-4  items-center relative w-full transition-transform ">
         {isOpen?(
-          <img src="./images/icon-close.svg" alt=""  className="brightness-0 size-6"
+          <img src="./images/icon-close.svg" alt=""  className="brightness-0 size-6 transition-transform duration-3"
           onClick={()=>setIsOpen(false)}
           />
         ):(
-          <img src="./images/icon-menu.svg" alt="" className="brightness-0 size-6 cursor-pointer" onClick={()=>setIsOpen(!isOpen)}/>
+          <img src="./images/icon-menu.svg" alt="" className="brightness-0 size-6 cursor-pointer transition-transform" onClick={()=>setIsOpen(!isOpen)}/>
         )}
 
 
-        <div className="absolute  top-full w-full z-20 mt-4 w-full ">
+        <div className="absolute  top-full w-full z-20 mt-4 w-full cursor-pointer">
         {isOpen &&(
           <div className=" bg-white shadow-lg p-4 space-y-4  divide-y divide-gray-300  ">
             <div className="flex justify-between items-center" onClick={()=>handletoggleMenu('explore')}>
@@ -38,7 +39,7 @@ export default function Nav(){
               </div>
             )}
 
-            <div className=" flex justify-between items-center" onClick={()=>handletoggleMenu('for companies')} >
+            <div className=" flex justify-between items-center cursor-pointer" onClick={()=>handletoggleMenu('for companies')} >
             <p>FOR COMPANIES</p>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  className={`transition-transform ${activeMenu ==='for companies' ?'rotate-90':''} lucide lucide-chevron-right-icon lucide-chevron-right`}><path d="m9 18 6-6-6-6"/></svg>
             </div>
@@ -48,15 +49,15 @@ export default function Nav(){
                 <li>TRAIN DEVELOPERS</li>
               </div>
             )}
-            <p>UNLOCK <span className=" font-bold text-white rounded px-2 py-1 bg-blue-800">PRO</span></p>
+            <p className="flex items-center gap-2 mt-4"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lock-keyhole-open-icon lucide-lock-keyhole-open"><circle cx="12" cy="16" r="1"/><rect width="18" height="12" x="3" y="10" rx="2"/><path d="M7 10V7a5 5 0 0 1 9.33-2.5"/></svg>UNLOCK <span className=" font-bold text-white rounded px-2 py-1 bg-blue-800">PRO</span></p>
 
           </div>
         )}
         </div>
         <img src="./images/favicon-32x32.png" alt="" />
       </div>
-      <button className="size-6 rounded-full ">
-       <img src="/logo192.png" alt="" />
+      <button className="bg-black p-2 rounded-full text-white font-bold text-sm w-1/2 flex gap-4 items-center justify-center">
+        LOG IN <FaGithub/>
       </button>
     </div>
   )
